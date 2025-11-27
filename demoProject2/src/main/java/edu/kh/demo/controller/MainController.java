38645f3eb1a231290bacf7957756887e9319bc36
ChangLae <chochanglae1223@gmail.com>
2025-11-28 02:24:19 +0900

    20251127
    2시30분까지 수업 다듣고 잔다!!:_)

--- demoProject2/src/main/java/edu/kh/demo/controller/MainController.java
+++ demoProject2/src/main/java/edu/kh/demo/controller/MainController.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // 요청/응답 제어 역할 명시 + Bean 등록
 public class MainController {
-
+	
 	// "/" 주소로 요청 시 main.html 파일로 forward
+	
 	@RequestMapping("/")
 	public String mainPage() {
 		
 		// forward : 요청 위임
-		// thymeleaf : Spring Boot에서 사용하는 템플릿
+		// thymeleaf : Spring Boot에서 사용하는 템플릿 엔진 (html 파일 사용)
+		
+		// thymeleaf를 이용한 html로 forward 시
+		// 사용되는 접두사, 접미사 존재
 		
 		// 접두사 : classpath:/templates/
 		// 접미사 : .html
@@ -19,4 +23,6 @@ public class MainController {
 		// -> classpath:/templates/common/main.html
 		return "common/main";
 	}
+	
+	
 }
